20bd6b0da4f91f4e59d941125b0575cdf5d69298
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-03-05 20:20:02 +0100

    Created python script to run chatbot

---
+++ NLP/Chatbot/Chatbot_.py
@@ -0,0 +1,63 @@
+import numpy as np
+import torch
+import json
+import random
+import torch.nn as nn
+from model_architect import MNet
+from nltk_utils import stem, tokenize, convert_and_pad
+
+# Loading saved variables
+with open( "intents.json", "r" ) as f:
+    intents = json.load( f )
+
+variables_pth = "variables.pt"
+variables = torch.load( variables_pth )
+
+# Initializing variables
+input_size = variables[ "input" ]
+output_size = variables[ "output" ]
+hidden_size = variables[ "hidden" ]
+all_words = variables[ "vocabulary" ]
+tags = variables[ "tags" ]
+model_state = variables[ "model_artefacts" ]
+
+
+model = MNet( input_size, hidden_size, output_size )
+model.load_state_dict( model_state )
+
+model.eval()
+
+bot_name = "Linden"
+print( "Hello, I am Linden, let's chat!" )
+print( "Type 'quit' to exit" )
+
+response_1 = ['Sorry, I do not understand.....',
+             'I think it is better you chat with a live agent, please hold..']
+while True:
+    sentence = input( 'You: ' )
+    if sentence.lower() == 'quit':
+        break
+
+    sentence = tokenize( sentence )
+    sentence = convert_and_pad( all_words, sentence )
+    sentence.reshape( 1, sentence.shape[0] )
+    sentence = torch.from_numpy( sentence )
+
+    output = model( sentence.float() )
+    probs = torch.softmax( output, dim=0 )
+    ps, idx = torch.sort( probs, descending=True )
+    ps = ps.detach().numpy()
+    idx = idx.detach().numpy()
+    ps, idx = ps[ 0 ], idx[ 0 ]
+    tag = tags[ idx ]
+
+    # Getting output probabilities
+    # probs = torch.softmax( output, dim=0 )
+    # prob = probs[ 0 ][ prediction.item() ]
+
+    if ps > 0.75:
+        for intent in intents[ "intents" ]:
+            if tag == intent[ "tag" ]:
+                print( f"{bot_name}: {random.choice(intent['responses'])}" )
+    else:
+        print( f"{bot_name}: {random.choice( response_1 )}" )
