9e776d1aad278b008e196859684dd6f483970635
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-08-16 22:28:46 +0100

    Modified training

--- NLP/Chatbot/model_architect.py
+++ NLP/Chatbot/model_architect.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn 
 
+
 # Building Architecture
 
 class MNet( nn.Module ):
@@ -8,11 +9,22 @@ class MNet( nn.Module ):
         super( MNet, self ).__init__()
         self.fc1 = nn.Linear( input_size, hidden_size )
         self.fc2 = nn.Linear( hidden_size, hidden_size )
+        self.fc2_1 = nn.Linear( hidden_size, hidden_size )
+        self.fc2_11 = nn.Linear( hidden_size, hidden_size )
         self.fc3 = nn.Linear( hidden_size, output )
         self.relu = nn.ReLU()
         
     def forward( self, x ):
         x = self.relu( self.fc1( x ) )
         x = self.relu( self.fc2( x ) )
+        x = self.relu( self.fc2_1( x ) )
+        x = self.relu( self.fc2_11( x ) )
         x = self.fc3( x )
         return x
+    
+"""##@Version control log
+    
+    1. commented out one layer of the architecture to reduce size
+    2. added layer 2_1
+    3. added layer 2_11
+    """
